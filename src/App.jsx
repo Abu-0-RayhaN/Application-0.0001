e2bfff1f2428d2cdc5d517deefead6a946de4b93
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-04-14 03:06:40 +0600

    navbar header brand

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <div className="gradient__bg">
-        {/* <Navbar /> */}
+        <Navbar />
         <Header />
       </div>
       <Brand />
