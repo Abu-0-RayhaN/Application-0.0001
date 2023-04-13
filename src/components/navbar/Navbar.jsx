46d1fe980df75961e6b65bf217beabd5390c0d81
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-04-14 00:24:44 +0600

    checking

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./navbar.css";
 import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import Logo from "../../assets/logo.svg";
 // BEM-> Block Element Modifier is the convention we are using for naming css clsses
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links-logo">
-          <img src="{logo}" alt="" />
+          <img src={Logo} alt="" />
         </div>
       </div>
     </div>
